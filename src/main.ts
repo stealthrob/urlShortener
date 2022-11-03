e5822dff6c78b0c85e8b7410d5d6977572265a0c
Stealthrob <nietoroberto2209@gmail.com>
2022-11-03 07:43:17 -0400

    port listening

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ async function bootstrap() {
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
   );
-  await app.listen(3000);
+  const port: number = parseInt(`${process.env.PORT}`) || 3000;
+await app.listen(port);
 }
 bootstrap();
