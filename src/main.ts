f2e2c4a312237ae747f58ea07aefc565b52d456c
Stealthrob <nietoroberto2209@gmail.com>
2022-11-03 10:58:09 -0400

    cross origin heroku fix

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
   );
   const port: number = parseInt(`${process.env.PORT}`) || 3000;
-  app.enableCors();
+  app.enableCors({origin: ['https://short-url-rn.herokuapp.com/', 'http://localhost:3000']});
   await app.listen(port);
 }
 bootstrap();
